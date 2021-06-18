85d1b0bba9224f7e7a4b46e861db1262f2ef221a
vongocman <vongocman0401@gmail.com>
2021-06-18 13:54:34 +0700

    MVC

--- news-website/app.js
+++ news-website/app.js
@@ -1,10 +1,16 @@
-const express = require('express');
+const express = require('express')
 const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const app = express();
-app.use(express.static(__dirname+'/public'));
+//app.use(express.static(__dirname+'/public'));
 app.use(morgan('dev'))
 const port = 3000
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use('/public',express.static('public'))
 
 app.engine('hbs',exphbs({
     defaultLayout: 'main.hbs'
@@ -14,6 +20,9 @@ app.set('view engine','hbs');
 app.get('/',function (req,res) {
    res.render('home')
 });
+
+app.use('/categories', require('./controllers/category.route'));
+
 
 app.listen(port,function () {
     console.log('start')
---
+++ news-website/controllers/category.route.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/', function (req,res) {
+    console.log("abc")
+    res.render('vwCategories/index')    
+});
+
+
+module.exports = router;
+
